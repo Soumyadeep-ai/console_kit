3c9daed8d4634f53464422b7c0cb70a4ada65dcf
Soumyadeep Pal <soumyadeep.pal@yabx.co>
2025-08-13 16:15:22 +0530

    reek fix setup_spec.rb

--- spec/console_kit/setup_spec.rb
+++ spec/console_kit/setup_spec.rb
@@ -13,7 +13,10 @@ RSpec.describe ConsoleKit::Setup do
   let(:context_class) do
     Class.new do
       class << self
-        attr_accessor :tenant_shard, :tenant_mongo_db, :partner_identifier
+        %i[tenant_shard tenant_mongo_db partner_identifier].each do |attr|
+          define_method(attr) { instance_variable_get("@#{attr}") }
+          define_method("#{attr}=") { |val| instance_variable_set("@#{attr}", val) }
+        end
       end
     end
   end
